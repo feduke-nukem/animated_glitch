622d86dcddfc06d732582390fb1b276dbc33f0f1
Plague Fox <plugfox@gmail.com>
2023-06-08 23:58:50 +0400

    Fix shaders

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,53 +1,61 @@
 import 'package:animated_glitch/animated_glitch.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => Future<void>(() async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await $initImage('assets/cyberpunk.jpg');
+      runApp(const App());
+    });
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Animated Glitch Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: AnimatedGlitchWithShader(
-        chance: 50,
-        colorChannelSpreadReduce: 30,
-        distortionSpreadReduce: 20,
-        frequency: 1,
-        level: 0.7,
-        glitchAmount: 3,
-        showColorChannel: true,
-        showDistortion: true,
-        child: Image.asset(
-          'assets/cyberpunk.jpg',
-          fit: BoxFit.cover,
-          height: double.infinity,
-          width: double.infinity,
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Animated Glitch Demo',
+        showPerformanceOverlay: true,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
-      ),
-    );
-  }
+        home: const HomePage(),
+      );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: SafeArea(
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+            ),
+            itemCount: 1000,
+            itemBuilder: (context, index) => const AnimatedGlitchWithShader(
+              chance: 50,
+              colorChannelSpreadReduce: 30,
+              distortionSpreadReduce: 20,
+              frequency: 1,
+              level: 0.7,
+              glitchAmount: 3,
+              showColorChannel: true,
+              showDistortion: true,
+              child: SizedBox.shrink(),
+              /* child: Image.asset(
+                'assets/cyberpunk.jpg',
+                fit: BoxFit.cover,
+                height: double.infinity,
+                width: double.infinity,
+              ), */
+            ),
+          ),
+        ),
+      );
 }
--- lib/src/widget/animated_glitch_with_shader.dart
+++ lib/src/widget/animated_glitch_with_shader.dart
@@ -1,21 +1,23 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_shaders/flutter_shaders.dart';
+import 'dart:ui' as ui;
 
-extension on bool {
-  double toFloat() => this ? 1.0 : 0.0;
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
+
+late ui.Image _$image;
+Future<ui.Image> _loadImage(String assetPath) async {
+  ByteData data = await rootBundle.load(assetPath);
+  ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List());
+  ui.FrameInfo fi = await codec.getNextFrame();
+
+  return fi.image;
 }
 
-class AnimatedGlitchWithShader extends StatefulWidget {
-  final double distortionSpreadReduce;
-  final double colorChannelSpreadReduce;
-  final double level;
-  final int glitchAmount;
-  final double frequency;
-  final int chance;
-  final bool showDistortion;
-  final bool showColorChannel;
-  final Widget child;
+Future<void> $initImage(String assetPath) async =>
+    _$image = await _loadImage(assetPath);
 
+class AnimatedGlitchWithShader extends StatefulWidget {
   const AnimatedGlitchWithShader({
     required this.chance,
     required this.colorChannelSpreadReduce,
@@ -31,6 +33,16 @@ class AnimatedGlitchWithShader extends StatefulWidget {
   }) : assert(glitchAmount <= 10,
             'glitchAmount must be less than or equal to 10');
 
+  final double distortionSpreadReduce;
+  final double colorChannelSpreadReduce;
+  final double level;
+  final int glitchAmount;
+  final double frequency;
+  final int chance;
+  final bool showDistortion;
+  final bool showColorChannel;
+  final Widget child;
+
   @override
   State<AnimatedGlitchWithShader> createState() =>
       _AnimatedGlitchWithShaderState();
@@ -38,63 +50,41 @@ class AnimatedGlitchWithShader extends StatefulWidget {
 
 class _AnimatedGlitchWithShaderState extends State<AnimatedGlitchWithShader>
     with SingleTickerProviderStateMixin {
-  double _time = 0.0;
+  static final Future<ui.FragmentShader> _shaderFuture = () async {
+    const shader = 'packages/animated_glitch/shader/glitch.frag';
+    final program = await ui.FragmentProgram.fromAsset(shader);
 
-  late final _ticker = createTicker((elapsed) {
-    setState(() {
-      _time += 0.0042;
-    });
-  });
+    return program.fragmentShader();
+  }();
+
+  late final ValueNotifier<double> _seed = ValueNotifier<double>(0.0);
+
+  late final Ticker _ticker =
+      createTicker((elapsed) => _seed.value = elapsed.inMicroseconds / 1000000);
 
   @override
-  Widget build(BuildContext context) {
-    return RepaintBoundary(
-      child: ShaderBuilder(
-        (context, shader, child) => AnimatedSampler(
-          (image, size, canvas) {
-            shader
-              // uTime
-              ..setFloat(0, _time)
-              // uResolution x
-              ..setFloat(1, size.width)
-              // uResolution y
-              ..setFloat(2, size.height)
-              // uDistortionOffsetDivisor
-              ..setFloat(3, widget.distortionSpreadReduce)
-              // uColorChannelOffsetDivisor
-              ..setFloat(4, widget.colorChannelSpreadReduce)
-              // uLevel
-              ..setFloat(5, widget.level)
-              // uFrequency
-              ..setFloat(6, widget.frequency)
-              // uChance
-              ..setFloat(7, widget.chance.toDouble())
-              // uShowDistortion
-              ..setFloat(8, widget.showDistortion.toFloat())
-              // uShowColorChannel
-              ..setFloat(9, widget.showColorChannel.toFloat())
-              // uShiftColorChannelsY
-              ..setFloat(10, 0)
-              // uShiftColorChannelsY
-              ..setFloat(11, 1)
-              // uGlitchAmount
-              ..setFloat(12, widget.glitchAmount.toDouble())
-              // uChannel0
-              ..setImageSampler(0, image);
-            canvas.save();
-            canvas.drawRect(
-              Offset.zero & size,
-              Paint()..shader = shader,
-            );
-            canvas.restore();
-          },
-          child: child!,
+  Widget build(BuildContext context) => RepaintBoundary(
+        child: FutureBuilder<ui.FragmentShader>(
+          future: _shaderFuture,
+          initialData: null,
+          builder: (context, snapshot) => CustomPaint(
+            painter: _AnimatedGlitchPainter$Shader(
+              seed: _seed,
+              shader: snapshot.data,
+              distortionSpreadReduce: widget.distortionSpreadReduce,
+              colorChannelSpreadReduce: widget.colorChannelSpreadReduce,
+              level: widget.level,
+              frequency: widget.frequency,
+              chance: widget.chance,
+              showDistortion: widget.showDistortion,
+              showColorChannel: widget.showColorChannel,
+              glitchAmount: widget.glitchAmount,
+              image: _$image,
+            ),
+            child: widget.child,
+          ),
         ),
-        assetKey: 'packages/animated_glitch/shader/glitch.frag',
-        child: widget.child,
-      ),
-    );
-  }
+      );
 
   @override
   void initState() {
@@ -107,4 +97,87 @@ class _AnimatedGlitchWithShaderState extends State<AnimatedGlitchWithShader>
     _ticker.dispose();
     super.dispose();
   }
+}
+
+class _AnimatedGlitchPainter$Shader extends CustomPainter {
+  _AnimatedGlitchPainter$Shader({
+    required this.seed,
+    required this.shader,
+    required this.distortionSpreadReduce,
+    required this.colorChannelSpreadReduce,
+    required this.level,
+    required this.frequency,
+    required this.chance,
+    required this.showDistortion,
+    required this.showColorChannel,
+    required this.glitchAmount,
+    required this.image,
+  }) : super(repaint: seed);
+
+  final ValueListenable<double> seed;
+  final ui.FragmentShader? shader;
+  final double distortionSpreadReduce;
+  final double colorChannelSpreadReduce;
+  final double level;
+  final double frequency;
+  final int chance;
+  final bool showDistortion;
+  final bool showColorChannel;
+  final int glitchAmount;
+  final ui.Image image;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (shader == null) {
+      canvas.drawRect(Offset.zero & size, Paint()..color = Colors.transparent);
+    }
+    final paint = Paint()
+      ..shader = (shader!
+        // uTime
+        ..setFloat(0, seed.value)
+        // uResolution x
+        ..setFloat(1, size.width)
+        // uResolution y
+        ..setFloat(2, size.height)
+        // uDistortionOffsetDivisor
+        ..setFloat(3, distortionSpreadReduce)
+        // uColorChannelOffsetDivisor
+        ..setFloat(4, colorChannelSpreadReduce)
+        // uLevel
+        ..setFloat(5, level)
+        // uFrequency
+        ..setFloat(6, frequency)
+        // uChance
+        ..setFloat(7, chance.toDouble())
+        // uShowDistortion
+        ..setFloat(8, showDistortion ? 1.0 : 0.0)
+        // uShowColorChannel
+        ..setFloat(9, showColorChannel ? 1.0 : 0.0)
+        // uShiftColorChannelsY
+        ..setFloat(10, 0)
+        // uShiftColorChannelsY
+        ..setFloat(11, 1)
+        // uGlitchAmount
+        ..setFloat(12, glitchAmount.toDouble())
+        // uChannel0
+        ..setImageSampler(0, image));
+    canvas.drawRect(Offset.zero & size, paint);
+
+    /* canvas.save();
+    canvas.drawRect(Offset.zero & size, Paint()..shader = shader);
+    canvas.restore(); */
+  }
+
+  @override
+  bool shouldRepaint(covariant _AnimatedGlitchPainter$Shader oldDelegate) =>
+      oldDelegate.seed.value != seed.value ||
+      oldDelegate.shader != shader ||
+      oldDelegate.distortionSpreadReduce != distortionSpreadReduce ||
+      oldDelegate.colorChannelSpreadReduce != colorChannelSpreadReduce ||
+      oldDelegate.level != level ||
+      oldDelegate.frequency != frequency ||
+      oldDelegate.chance != chance ||
+      oldDelegate.showDistortion != showDistortion ||
+      oldDelegate.showColorChannel != showColorChannel ||
+      oldDelegate.glitchAmount != glitchAmount;
 }
